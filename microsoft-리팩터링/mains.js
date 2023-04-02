d66b1a6a5900bed39e5fdb93985d2e2bfccda261
chicken-tender <ygm815@naver.com>
2023-04-02 09:37:32 +0900

    메인 이미지 슬라이드 구현

--- microsoft-리팩터링/mains.js
+++ microsoft-리팩터링/mains.js
@@ -6,3 +6,23 @@ toggleBtn.addEventListener('click', () => {
   menu.classList.toggle('active');
   icons.classList.toggle('active');
 });
+
+const slides = document.querySelectorAll('.img-item');
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 8000);
+}
